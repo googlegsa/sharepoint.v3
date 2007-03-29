41f045fdd43906c1fe98fd0732eae1e088788bcd
Meghna Dhar <meghna@google.com>
2007-03-29 23:36:41 +0000

    Bob's tests for state code.

--- projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/SharepointQueryTraversalManagerTest.java
+++ projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/SharepointQueryTraversalManagerTest.java
@@ -1,71 +1,177 @@
-// Copyright (C) 2007 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.enterprise.connector.sharepoint;
 
-
-
+import com.google.enterprise.connector.sharepoint.SharepointConnector;
+import com.google.enterprise.connector.sharepoint.SharepointQueryTraversalManager;
+import com.google.enterprise.connector.sharepoint.client.SharepointClient;
+import com.google.enterprise.connector.sharepoint.client.SharepointClientContext;
+import com.google.enterprise.connector.sharepoint.state.GlobalState;
+import com.google.enterprise.connector.sharepoint.state.GlobalStateInitializer;
+import com.google.enterprise.connector.sharepoint.state.ListState;
+import com.google.enterprise.connector.spi.Property;
+import com.google.enterprise.connector.spi.PropertyMap;
 import com.google.enterprise.connector.spi.RepositoryException;
-import com.google.enterprise.connector.spi.Session;
-import com.google.enterprise.connector.spi.SimpleResultSet;
+import com.google.enterprise.connector.spi.ResultSet;
+import com.google.enterprise.connector.spi.SpiConstants;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.util.Iterator;
 
 
 public class SharepointQueryTraversalManagerTest extends TestCase {
-
   final String sharepointUrl = "http://entpoint05.corp.google.com/unittest";
   final String domain = "ent-qa-d3";
   final String host = "entpoint05.corp.google.com";
   final int port = 80;
   final String username = "testing";
   final String password = "g00gl3";
+  private SharepointClient sharepointClient;
+  private SharepointConnector connector;
+  private SharepointQueryTraversalManager manager;
+  
+  public void setUp() throws Exception {
+    GlobalStateInitializer.init();
+    SharepointClientContext sharepointClientContext = new 
+      SharepointClientContext(sharepointUrl, domain, username, password);
+    sharepointClient = new SharepointClient(sharepointClientContext);
+    connector = new SharepointConnector(sharepointUrl, 
+        domain, username, password);
+    manager = new SharepointQueryTraversalManager(connector, 
+        sharepointClientContext);
+    
+    // important: make it forget whatever it read in from a left-over file
+    manager.forgetStateForUnittest();
+    super.setUp();    
+  }
   
   /**
-   * Test method for
-   * {@link com.google.enterprise.connector.traversal.QueryTraverser
-   * #runBatch(int)}.
-   * @throws InterruptedException, RepositoryException
+   * Basic traversal; no hints, no checkpointing
    */
-  public final void testRunBatch() throws InterruptedException, 
-    RepositoryException {      
-    runTestBatches(5); 
-    runTestBatches(6);
-    runTestBatches(7);
-    runTestBatches(8);
-    runTestBatches(9);
-    runTestBatches(10);
-  }
-
-  private void runTestBatches(int batchSize) throws InterruptedException, 
-    RepositoryException {
-    SharepointConnector sharepointConnector = new SharepointConnector(
-                        sharepointUrl, domain, username, password);
-    Session sess = sharepointConnector.login();
-    String connectorName = "sharepoint-connector";
-    SharepointQueryTraversalManager qtm = 
-        (SharepointQueryTraversalManager) sess.getQueryTraversalManager();
+  public void testBasic() {
     try {
-      System.out.println("\nRunning batch test batchsize " + batchSize);
-      SimpleResultSet rs = (SimpleResultSet) qtm.startTraversal();
-      Assert.assertEquals(7, rs.size());
-    } catch (Exception e) {      
-      fail(e.toString());
+      ResultSet rs = manager.startTraversal();
+      boolean found = false;
+      int numDocs = 0;
+      for (Iterator<PropertyMap> it = rs.iterator(); it.hasNext(); ) {
+        PropertyMap pm = it.next();
+        Property urlProp = pm.getProperty(SpiConstants.PROPNAME_SEARCHURL);
+        String url = urlProp.getValue().getString();
+        if (url.equals("http://entpoint05.corp.google.com:80/unittest/" +
+        "TestDocumentLibrary/TestFolder/TestFolder1/webDav.doc")) {
+          found = true;
+        }
+        numDocs++;         
+       }
+      assertTrue(found);
+      assertEquals(numDocs, 7);
+    } catch (RepositoryException e) {
+      e.printStackTrace();
+      fail("caught exception " + e.toString());
     }
-  }  
+  }
+  
+
+  public void testHints() {
+    try {
+      manager.setBatchHint(3);
+      ResultSet rs = manager.startTraversal();
+      boolean found = false;
+      int numDocs = 0;
+      for (Iterator<PropertyMap> it = rs.iterator(); it.hasNext(); ) {
+        PropertyMap pm = it.next();
+        Property urlProp = pm.getProperty(SpiConstants.PROPNAME_SEARCHURL);
+        String url = urlProp.getValue().getString();
+        System.out.println(url + " from " + 
+            pm.getProperty(SharepointClient.LIST_GUID).getValue().getString());
+        if (url.equals("http://entpoint05.corp.google.com:80/unittest" + 
+            "/Shared Documents/sync.doc")) {
+          found = true;
+        }
+        numDocs++;         
+      }
+      assertTrue(found);
+      assertEquals(numDocs, 3); 
+      
+      // now, get the next bunch:
+      rs = manager.startTraversal();
+      found = false;
+      numDocs = 0;
+      for (Iterator<PropertyMap> it = rs.iterator(); it.hasNext(); ) {
+        PropertyMap pm = it.next();
+        Property urlProp = pm.getProperty(SpiConstants.PROPNAME_SEARCHURL);
+        String url = urlProp.getValue().getString();
+        System.out.println(url + " from " + 
+            pm.getProperty(SharepointClient.LIST_GUID).getValue().getString());
+        if (url.equals("http://entpoint05.corp.google.com:80/unittest" +
+            "/TestDocumentLibrary/TestFolder/webDav.doc")) {
+          found = true;
+        }
+        numDocs++;         
+      }
+      assertTrue(found);
+      assertEquals(numDocs, 4); 
+    } catch (RepositoryException e) {
+      e.printStackTrace();
+      fail("caught exception " + e.toString());
+    }
+  }
+  
+  /**
+   * similar to above, but we take a checkpoint in the middle. Results should
+   * be the same.
+   */
+  public void testHintsWithCheckpoint() {
+    System.out.println("\n\ntestHintsWithCheckpoint\n\n");
+    try {
+      manager.setBatchHint(3);
+      ResultSet rs = manager.startTraversal();
+      boolean found = false;
+      int numDocs = 0;
+      PropertyMap pm = null;
+      for (Iterator<PropertyMap> it = rs.iterator(); it.hasNext(); ) {
+        pm = it.next();
+        Property urlProp = pm.getProperty(SpiConstants.PROPNAME_SEARCHURL);
+        String url = urlProp.getValue().getString();
+        System.out.println(url + " from " + 
+            pm.getProperty(SharepointClient.LIST_GUID).getValue().getString());
+        if (url.equals("http://entpoint05.corp.google.com:80/unittest" + 
+            "/Shared Documents/sync.doc")) {
+          found = true;
+        }
+        numDocs++;         
+      }
+      assertTrue(found);
+      assertEquals(numDocs, 3); 
+      
+      // use last item for the checkpoint()
+      System.out.println("Before checkpoint: ");
+      System.out.println(manager.globalState.saveStateXML());
+      manager.checkpoint(pm);
+      System.out.println("\n\nAfter checkpoint: ");
+      System.out.println(manager.globalState.saveStateXML());
+      
+      // now, get the next bunch:
+      rs = manager.startTraversal();
+      found = false;
+      numDocs = 0;
+      for (Iterator<PropertyMap> it = rs.iterator(); it.hasNext(); ) {
+        pm = it.next();
+        Property urlProp = pm.getProperty(SpiConstants.PROPNAME_SEARCHURL);
+        String url = urlProp.getValue().getString();
+        System.out.println(url + " from " + 
+            pm.getProperty(SharepointClient.LIST_GUID).getValue().getString());
+        if (url.equals("http://entpoint05.corp.google.com:80/unittest" +
+            "/TestDocumentLibrary/TestFolder/webDav.doc")) {
+          found = true;
+        }
+        numDocs++;         
+      }
+      assertTrue(found);
+      assertEquals(numDocs, 4); 
+    } catch (RepositoryException e) {
+      e.printStackTrace();
+      fail("caught exception " + e.toString());
+    }
+  }
+  
 }
