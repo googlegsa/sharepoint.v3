d7f97ca796316eaec7ffa4a16d3c7f713761e1e6
th.nitendra <th.nitendra@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2009-07-14 14:24:45 +0000

    Creating branch for GSP2.0 containing GSS-installer code base

---
+++ branches/2.0.x/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/userprofileservice/GetUserProfileCountResponse.java
@@ -0,0 +1,115 @@
+/**
+ * GetUserProfileCountResponse.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.userprofileservice;
+
+public class GetUserProfileCountResponse  implements java.io.Serializable {
+    private long getUserProfileCountResult;
+
+    public GetUserProfileCountResponse() {
+    }
+
+    public GetUserProfileCountResponse(
+           long getUserProfileCountResult) {
+           this.getUserProfileCountResult = getUserProfileCountResult;
+    }
+
+
+    /**
+     * Gets the getUserProfileCountResult value for this GetUserProfileCountResponse.
+     * 
+     * @return getUserProfileCountResult
+     */
+    public long getGetUserProfileCountResult() {
+        return getUserProfileCountResult;
+    }
+
+
+    /**
+     * Sets the getUserProfileCountResult value for this GetUserProfileCountResponse.
+     * 
+     * @param getUserProfileCountResult
+     */
+    public void setGetUserProfileCountResult(long getUserProfileCountResult) {
+        this.getUserProfileCountResult = getUserProfileCountResult;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof GetUserProfileCountResponse)) return false;
+        GetUserProfileCountResponse other = (GetUserProfileCountResponse) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            this.getUserProfileCountResult == other.getGetUserProfileCountResult();
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        _hashCode += new Long(getGetUserProfileCountResult()).hashCode();
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(GetUserProfileCountResponse.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", ">GetUserProfileCountResponse"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("getUserProfileCountResult");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "GetUserProfileCountResult"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
