c2e81c9f85e3de21f5dc52ee7e1749c329a1daaf
shashankguru <shashankguru@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2009-08-13 15:39:51 +0000

    Tagging as 2.0.0 after adding GSS 2.0.0 x64 installer

---
+++ tags/2.0.0/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/userprofileservice/UpdateColleaguePrivacy.java
@@ -0,0 +1,192 @@
+/**
+ * UpdateColleaguePrivacy.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.userprofileservice;
+
+public class UpdateColleaguePrivacy  implements java.io.Serializable {
+    private java.lang.String accountName;
+
+    private java.lang.String colleagueAccountName;
+
+    private com.google.enterprise.connector.sharepoint.generated.userprofileservice.Privacy newPrivacy;
+
+    public UpdateColleaguePrivacy() {
+    }
+
+    public UpdateColleaguePrivacy(
+           java.lang.String accountName,
+           java.lang.String colleagueAccountName,
+           com.google.enterprise.connector.sharepoint.generated.userprofileservice.Privacy newPrivacy) {
+           this.accountName = accountName;
+           this.colleagueAccountName = colleagueAccountName;
+           this.newPrivacy = newPrivacy;
+    }
+
+
+    /**
+     * Gets the accountName value for this UpdateColleaguePrivacy.
+     * 
+     * @return accountName
+     */
+    public java.lang.String getAccountName() {
+        return accountName;
+    }
+
+
+    /**
+     * Sets the accountName value for this UpdateColleaguePrivacy.
+     * 
+     * @param accountName
+     */
+    public void setAccountName(java.lang.String accountName) {
+        this.accountName = accountName;
+    }
+
+
+    /**
+     * Gets the colleagueAccountName value for this UpdateColleaguePrivacy.
+     * 
+     * @return colleagueAccountName
+     */
+    public java.lang.String getColleagueAccountName() {
+        return colleagueAccountName;
+    }
+
+
+    /**
+     * Sets the colleagueAccountName value for this UpdateColleaguePrivacy.
+     * 
+     * @param colleagueAccountName
+     */
+    public void setColleagueAccountName(java.lang.String colleagueAccountName) {
+        this.colleagueAccountName = colleagueAccountName;
+    }
+
+
+    /**
+     * Gets the newPrivacy value for this UpdateColleaguePrivacy.
+     * 
+     * @return newPrivacy
+     */
+    public com.google.enterprise.connector.sharepoint.generated.userprofileservice.Privacy getNewPrivacy() {
+        return newPrivacy;
+    }
+
+
+    /**
+     * Sets the newPrivacy value for this UpdateColleaguePrivacy.
+     * 
+     * @param newPrivacy
+     */
+    public void setNewPrivacy(com.google.enterprise.connector.sharepoint.generated.userprofileservice.Privacy newPrivacy) {
+        this.newPrivacy = newPrivacy;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof UpdateColleaguePrivacy)) return false;
+        UpdateColleaguePrivacy other = (UpdateColleaguePrivacy) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.accountName==null && other.getAccountName()==null) || 
+             (this.accountName!=null &&
+              this.accountName.equals(other.getAccountName()))) &&
+            ((this.colleagueAccountName==null && other.getColleagueAccountName()==null) || 
+             (this.colleagueAccountName!=null &&
+              this.colleagueAccountName.equals(other.getColleagueAccountName()))) &&
+            ((this.newPrivacy==null && other.getNewPrivacy()==null) || 
+             (this.newPrivacy!=null &&
+              this.newPrivacy.equals(other.getNewPrivacy())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getAccountName() != null) {
+            _hashCode += getAccountName().hashCode();
+        }
+        if (getColleagueAccountName() != null) {
+            _hashCode += getColleagueAccountName().hashCode();
+        }
+        if (getNewPrivacy() != null) {
+            _hashCode += getNewPrivacy().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(UpdateColleaguePrivacy.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", ">UpdateColleaguePrivacy"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("accountName");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "accountName"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("colleagueAccountName");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "colleagueAccountName"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("newPrivacy");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "newPrivacy"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "Privacy"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
---
+++ tags/2.0.0/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/wsclient/ListsWSTest.java
@@ -0,0 +1,117 @@
+//Copyright 2007 Google Inc.
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+package com.google.enterprise.connector.sharepoint.wsclient;
+
+import java.net.MalformedURLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import com.google.enterprise.connector.sharepoint.TestConfiguration;
+import com.google.enterprise.connector.sharepoint.client.SPConstants;
+import com.google.enterprise.connector.sharepoint.client.SharepointClientContext;
+import com.google.enterprise.connector.sharepoint.spiimpl.SPDocument;
+import com.google.enterprise.connector.sharepoint.state.GlobalState;
+import com.google.enterprise.connector.sharepoint.state.ListState;
+import com.google.enterprise.connector.sharepoint.state.WebState;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+public class ListsWSTest extends TestCase {
+	SharepointClientContext sharepointClientContext;
+	ListsWS listWS;
+	ListState testList;
+	Calendar lastModified;
+	String lastItemID;
+	String lastItemURL;
+		
+	protected void setUp() throws Exception {
+		System.out.println("\n...Setting Up...");		
+		System.out.println("Initializing SharepointClientContext ...");
+		this.sharepointClientContext = new SharepointClientContext(TestConfiguration.sharepointUrl, TestConfiguration.domain, 
+				  TestConfiguration.username, TestConfiguration.Password, TestConfiguration.googleConnectorWorkDir, 
+				  TestConfiguration.includedURls, TestConfiguration.excludedURls, TestConfiguration.mySiteBaseURL, 
+				  TestConfiguration.AliasMap, TestConfiguration.feedType);		
+
+		assertNotNull(this.sharepointClientContext);
+		sharepointClientContext.setIncluded_metadata(TestConfiguration.whiteList);
+        sharepointClientContext.setExcluded_metadata(TestConfiguration.blackList);		
+		
+		System.out.println("Initializing ListsWS ...");
+		this.listWS = new ListsWS(this.sharepointClientContext);
+		
+		System.out.println("Creating test List ...");
+		final SiteDataWS siteDataWS = new SiteDataWS(this.sharepointClientContext);
+		
+		final GlobalState state = new GlobalState(TestConfiguration.googleConnectorWorkDir, SPConstants.CONTENT_FEED);
+		WebState ws = state.makeWebState(sharepointClientContext, TestConfiguration.ParentWebURL);
+		
+		final List listCollection = siteDataWS.getNamedLists(ws);
+		
+		assertNotNull(listCollection);
+		for (int i = 0; i < listCollection.size(); i++) {
+			final ListState baseList = (ListState) listCollection.get(i);
+			if(baseList.getPrimaryKey().equals(TestConfiguration.BaseListID)){
+				this.testList = baseList;				
+			}
+		}
+		System.out.println("Test List being used: "+this.testList.getPrimaryKey());		
+	}
+	
+	public final void testListsWS() throws Throwable {
+		System.out.println("Testing ListsWS(SharepointClientContext, siteName)...");
+		sharepointClientContext.setSiteURL(TestConfiguration.ParentWebURL);
+		this.listWS = new ListsWS(this.sharepointClientContext);
+		assertNotNull(this.listWS);
+		System.out.println("[ ListsWS(SharepointClientContext, siteName) ] Test Passed");
+	}
+	
+	public void testGetAttachments() throws MalformedURLException, RepositoryException {
+		System.out.println("Testing getAttachments()...");
+		final SPDocument doc = new SPDocument("1", "url1",new GregorianCalendar(2007, 1, 1), SPConstants.NO_AUTHOR, SPConstants.NO_OBJTYPE, SPConstants.PARENT_WEB_TITLE, SPConstants.CONTENT_FEED,SPConstants.SP2007);
+		final List items = this.listWS.getAttachments(this.testList, doc);
+		assertNotNull(items);
+		System.out.println("[ getAttachments() ] Test Passed.");	
+	}	
+	
+	public void testGetFolderHierarchy() throws MalformedURLException, RepositoryException {
+		System.out.println("Testing getFolderHierarchy()...");
+		final List items = this.listWS.getFolderHierarchy(this.testList, "", null);
+		assertNotNull(items);
+		System.out.println("[ getFolderHierarchy() ] Test Passed.");		
+	}
+	
+	public void testGetListItemsAtFolderLevel() throws MalformedURLException, RepositoryException {
+		System.out.println("Testing getListItemsAtFolderLevel()...");
+		final List items = this.listWS.getListItemsAtFolderLevel(this.testList, null, null);
+		assertNotNull(items);
+		System.out.println("[ getListItemsAtFolderLevel() ] Test Passed.");		
+	}
+	
+	public void testGetListItems() throws MalformedURLException, RepositoryException {
+		System.out.println("Testing getListItems()...");
+		final List items = this.listWS.getListItems(this.testList, null, null, null);
+		assertNotNull(items);
+		System.out.println("[ getListItems() ] Test Passed.");		
+	}
+	
+	public void testGetListItemChangesSinceToken() throws MalformedURLException, RepositoryException {
+		System.out.println("Testing getListItemChangesSinceToken()...");
+		final List items = this.listWS.getListItemChangesSinceToken(this.testList, null, null, null);
+		assertNotNull(items);
+		System.out.println("[ getListItemChangesSinceToken() ] Test Passed.");		
+	}
+}
