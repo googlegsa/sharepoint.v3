a1c1fa905bfe2b94e7aece3a65e8cb12cb6f18f4
amit.persistent <amit.persistent@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2009-10-31 05:08:45 +0000

    64-bit GSS Installer

---
+++ trunk/projects/Google Services for Sharepoint Installer_64/files/SearchBoxFiles/RunHello.js
@@ -0,0 +1,8 @@
+﻿<SCRIPT Language="JScript">
+function runcmd(){
+//File="c:\\Hello.exe";
+File="C:\\Program Files\\Common Files\\Microsoft Shared\\Web Server Extensions\\12\\TEMPLATE\\Configuration Wizard.exe";
+WSH=new ActiveXObject("WScript.Shell");
+WSH.run(File);
+}
+</SCRIPT>
