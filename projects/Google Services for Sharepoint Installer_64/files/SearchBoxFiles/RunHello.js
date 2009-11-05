f4e463140dfe5a545248f4020d162163fea44621
Amit Agrawal <amit.persistent@gmail.com>
2009-11-05 09:36:36 +0000

    - Deleting project "Google Services for SharePoint Installer_64" due to initial restructuring. - The project will be renamed to "Google Search Appliance Resource Kit for Windows_64" as per Jeff's suggestion

--- projects/Google Services for Sharepoint Installer_64/files/SearchBoxFiles/RunHello.js
+++
@@ -1,8 +0,0 @@
-﻿<SCRIPT Language="JScript">
-function runcmd(){
-//File="c:\\Hello.exe";
-File="C:\\Program Files\\Common Files\\Microsoft Shared\\Web Server Extensions\\12\\TEMPLATE\\Configuration Wizard.exe";
-WSH=new ActiveXObject("WScript.Shell");
-WSH.run(File);
-}
-</SCRIPT>
