0cb5fa2d5374a74f4a79987d79297aa0fc359ad4
pankajchohan <pankajchohan@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2009-11-06 13:11:16 +0000

    Copying trunk changes of r436 to branch 2.4.x

---
+++ RC2/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/gssitediscovery/SiteDiscoverySoap_PortType.java
@@ -0,0 +1,13 @@
+/**
+ * SiteDiscoverySoap_PortType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.gssitediscovery;
+
+public interface SiteDiscoverySoap_PortType extends java.rmi.Remote {
+    public java.lang.String checkConnectivity() throws java.rmi.RemoteException;
+    public java.lang.Object[] getAllSiteCollectionFromAllWebApps() throws java.rmi.RemoteException;
+}
