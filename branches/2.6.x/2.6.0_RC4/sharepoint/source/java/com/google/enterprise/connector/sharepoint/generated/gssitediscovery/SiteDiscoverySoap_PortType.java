9401a47ad17ed571974173a403234dcb2cffa6a3
th.nitendra <th.nitendra@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2010-06-11 15:45:54 +0000

    Creating 2.6.0 RC4 release candidate

---
+++ branches/2.6.x/2.6.0_RC4/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/gssitediscovery/SiteDiscoverySoap_PortType.java
@@ -0,0 +1,17 @@
+/**
+ * SiteDiscoverySoap_PortType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.gssitediscovery;
+
+public interface SiteDiscoverySoap_PortType extends java.rmi.Remote {
+    public java.lang.String checkConnectivity() throws java.rmi.RemoteException;
+    public java.lang.Object[] getAllSiteCollectionFromAllWebApps() throws java.rmi.RemoteException;
+    public com.google.enterprise.connector.sharepoint.generated.gssitediscovery.WebCrawlInfo getWebCrawlInfo() throws java.rmi.RemoteException;
+    public com.google.enterprise.connector.sharepoint.generated.gssitediscovery.WebCrawlInfo[] getWebCrawlInfoInBatch(java.lang.String[] webUrls) throws java.rmi.RemoteException;
+    public com.google.enterprise.connector.sharepoint.generated.gssitediscovery.ListCrawlInfo[] getListCrawlInfo(java.lang.String[] listGuids) throws java.rmi.RemoteException;
+    public boolean isCrawlableList(java.lang.String listGUID) throws java.rmi.RemoteException;
+}
