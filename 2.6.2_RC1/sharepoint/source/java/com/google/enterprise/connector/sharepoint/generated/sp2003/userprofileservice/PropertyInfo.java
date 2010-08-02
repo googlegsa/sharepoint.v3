a27cd9253d3b1e56b4825b41fae2e5816a6df03b
Nitendra Thakur <th.nitendra@gmail.com>
2010-08-02 11:51:48 +0000

    Creating 2.6.2_RC1 release candidate branch

---
+++ 2.6.2_RC1/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/sp2003/userprofileservice/PropertyInfo.java
@@ -0,0 +1,194 @@
+/**
+ * PropertyInfo.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice;
+
+public class PropertyInfo  implements java.io.Serializable {
+    private java.lang.String name;
+
+    private java.lang.String displayName;
+
+    private java.lang.String type;
+
+    public PropertyInfo() {
+    }
+
+    public PropertyInfo(
+           java.lang.String name,
+           java.lang.String displayName,
+           java.lang.String type) {
+           this.name = name;
+           this.displayName = displayName;
+           this.type = type;
+    }
+
+
+    /**
+     * Gets the name value for this PropertyInfo.
+     *
+     * @return name
+     */
+    public java.lang.String getName() {
+        return name;
+    }
+
+
+    /**
+     * Sets the name value for this PropertyInfo.
+     *
+     * @param name
+     */
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Gets the displayName value for this PropertyInfo.
+     *
+     * @return displayName
+     */
+    public java.lang.String getDisplayName() {
+        return displayName;
+    }
+
+
+    /**
+     * Sets the displayName value for this PropertyInfo.
+     *
+     * @param displayName
+     */
+    public void setDisplayName(java.lang.String displayName) {
+        this.displayName = displayName;
+    }
+
+
+    /**
+     * Gets the type value for this PropertyInfo.
+     *
+     * @return type
+     */
+    public java.lang.String getType() {
+        return type;
+    }
+
+
+    /**
+     * Sets the type value for this PropertyInfo.
+     *
+     * @param type
+     */
+    public void setType(java.lang.String type) {
+        this.type = type;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof PropertyInfo)) return false;
+        PropertyInfo other = (PropertyInfo) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true &&
+            ((this.name==null && other.getName()==null) ||
+             (this.name!=null &&
+              this.name.equals(other.getName()))) &&
+            ((this.displayName==null && other.getDisplayName()==null) ||
+             (this.displayName!=null &&
+              this.displayName.equals(other.getDisplayName()))) &&
+            ((this.type==null && other.getType()==null) ||
+             (this.type!=null &&
+              this.type.equals(other.getType())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getName() != null) {
+            _hashCode += getName().hashCode();
+        }
+        if (getDisplayName() != null) {
+            _hashCode += getDisplayName().hashCode();
+        }
+        if (getType() != null) {
+            _hashCode += getType().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(PropertyInfo.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "PropertyInfo"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("name");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "Name"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("displayName");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "DisplayName"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("type");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "Type"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType,
+           java.lang.Class _javaType,
+           javax.xml.namespace.QName _xmlType) {
+        return
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType,
+           java.lang.Class _javaType,
+           javax.xml.namespace.QName _xmlType) {
+        return
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
