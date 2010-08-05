56068da1b889d084cc3695dfc8fcd07211acc859
Nitendra Thakur <th.nitendra@gmail.com>
2010-08-05 13:02:30 +0000

    Creating 2.6.2_RC2 release candidate branch

---
+++ 2.6.2_RC2/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/gsbulkauthorization/BulkAuthorization.java
@@ -0,0 +1,21 @@
+/**
+ * BulkAuthorization.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.gsbulkauthorization;
+
+public interface BulkAuthorization extends javax.xml.rpc.Service {
+    public java.lang.String getBulkAuthorizationSoap12Address();
+
+    public com.google.enterprise.connector.sharepoint.generated.gsbulkauthorization.BulkAuthorizationSoap_PortType getBulkAuthorizationSoap12() throws javax.xml.rpc.ServiceException;
+
+    public com.google.enterprise.connector.sharepoint.generated.gsbulkauthorization.BulkAuthorizationSoap_PortType getBulkAuthorizationSoap12(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+    public java.lang.String getBulkAuthorizationSoapAddress();
+
+    public com.google.enterprise.connector.sharepoint.generated.gsbulkauthorization.BulkAuthorizationSoap_PortType getBulkAuthorizationSoap() throws javax.xml.rpc.ServiceException;
+
+    public com.google.enterprise.connector.sharepoint.generated.gsbulkauthorization.BulkAuthorizationSoap_PortType getBulkAuthorizationSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
