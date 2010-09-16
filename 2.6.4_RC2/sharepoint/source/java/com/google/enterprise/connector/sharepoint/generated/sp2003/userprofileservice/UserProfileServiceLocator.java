4e0475581e12ef28701ffcbe66355b6225f10672
Nitendra Thakur <th.nitendra@gmail.com>
2010-09-16 09:15:58 +0000

    Creating RC2 branch for 2.6.4

---
+++ 2.6.4_RC2/sharepoint/source/java/com/google/enterprise/connector/sharepoint/generated/sp2003/userprofileservice/UserProfileServiceLocator.java
@@ -0,0 +1,146 @@
+/**
+ * UserProfileServiceLocator.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice;
+
+public class UserProfileServiceLocator extends org.apache.axis.client.Service implements com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileService {
+
+/**
+ * User Profile Service
+ */
+
+    public UserProfileServiceLocator() {
+    }
+
+
+    public UserProfileServiceLocator(org.apache.axis.EngineConfiguration config) {
+        super(config);
+    }
+
+    public UserProfileServiceLocator(java.lang.String wsdlLoc, javax.xml.namespace.QName sName) throws javax.xml.rpc.ServiceException {
+        super(wsdlLoc, sName);
+    }
+
+    // Use to get a proxy class for UserProfileServiceSoap
+    private java.lang.String UserProfileServiceSoap_address = "http://172.25.234.129/_vti_bin/UserProfileService.asmx";
+
+    public java.lang.String getUserProfileServiceSoapAddress() {
+        return UserProfileServiceSoap_address;
+    }
+
+    // The WSDD service name defaults to the port name.
+    private java.lang.String UserProfileServiceSoapWSDDServiceName = "UserProfileServiceSoap";
+
+    public java.lang.String getUserProfileServiceSoapWSDDServiceName() {
+        return UserProfileServiceSoapWSDDServiceName;
+    }
+
+    public void setUserProfileServiceSoapWSDDServiceName(java.lang.String name) {
+        UserProfileServiceSoapWSDDServiceName = name;
+    }
+
+    public com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_PortType getUserProfileServiceSoap() throws javax.xml.rpc.ServiceException {
+       java.net.URL endpoint;
+        try {
+            endpoint = new java.net.URL(UserProfileServiceSoap_address);
+        }
+        catch (java.net.MalformedURLException e) {
+            throw new javax.xml.rpc.ServiceException(e);
+        }
+        return getUserProfileServiceSoap(endpoint);
+    }
+
+    public com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_PortType getUserProfileServiceSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException {
+        try {
+            com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_BindingStub _stub = new com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_BindingStub(portAddress, this);
+            _stub.setPortName(getUserProfileServiceSoapWSDDServiceName());
+            return _stub;
+        }
+        catch (org.apache.axis.AxisFault e) {
+            return null;
+        }
+    }
+
+    public void setUserProfileServiceSoapEndpointAddress(java.lang.String address) {
+        UserProfileServiceSoap_address = address;
+    }
+
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    public java.rmi.Remote getPort(Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        try {
+            if (com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_PortType.class.isAssignableFrom(serviceEndpointInterface)) {
+                com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_BindingStub _stub = new com.google.enterprise.connector.sharepoint.generated.sp2003.userprofileservice.UserProfileServiceSoap_BindingStub(new java.net.URL(UserProfileServiceSoap_address), this);
+                _stub.setPortName(getUserProfileServiceSoapWSDDServiceName());
+                return _stub;
+            }
+        }
+        catch (java.lang.Throwable t) {
+            throw new javax.xml.rpc.ServiceException(t);
+        }
+        throw new javax.xml.rpc.ServiceException("There is no stub implementation for the interface:  " + (serviceEndpointInterface == null ? "null" : serviceEndpointInterface.getName()));
+    }
+
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    public java.rmi.Remote getPort(javax.xml.namespace.QName portName, Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        if (portName == null) {
+            return getPort(serviceEndpointInterface);
+        }
+        java.lang.String inputPortName = portName.getLocalPart();
+        if ("UserProfileServiceSoap".equals(inputPortName)) {
+            return getUserProfileServiceSoap();
+        }
+        else  {
+            java.rmi.Remote _stub = getPort(serviceEndpointInterface);
+            ((org.apache.axis.client.Stub) _stub).setPortName(portName);
+            return _stub;
+        }
+    }
+
+    public javax.xml.namespace.QName getServiceName() {
+        return new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "UserProfileService");
+    }
+
+    private java.util.HashSet ports = null;
+
+    public java.util.Iterator getPorts() {
+        if (ports == null) {
+            ports = new java.util.HashSet();
+            ports.add(new javax.xml.namespace.QName("http://microsoft.com/webservices/SharePointPortalServer/UserProfileService", "UserProfileServiceSoap"));
+        }
+        return ports.iterator();
+    }
+
+    /**
+    * Set the endpoint address for the specified port name.
+    */
+    public void setEndpointAddress(java.lang.String portName, java.lang.String address) throws javax.xml.rpc.ServiceException {
+
+if ("UserProfileServiceSoap".equals(portName)) {
+            setUserProfileServiceSoapEndpointAddress(address);
+        }
+        else
+{ // Unknown Port Name
+            throw new javax.xml.rpc.ServiceException(" Cannot set Endpoint Address for Unknown Port" + portName);
+        }
+    }
+
+    /**
+    * Set the endpoint address for the specified port name.
+    */
+    public void setEndpointAddress(javax.xml.namespace.QName portName, java.lang.String address) throws javax.xml.rpc.ServiceException {
+        setEndpointAddress(portName.getLocalPart(), address);
+    }
+
+}
