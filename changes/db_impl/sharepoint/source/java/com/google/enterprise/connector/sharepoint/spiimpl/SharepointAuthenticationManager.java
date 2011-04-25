bce9ced89adb8942374d659438d3e5d30419dd8e
s.nagesh10@gmail.com <s.nagesh10@gmail.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2011-04-25 13:59:27 +0000

    #Feature or Bug: Implementation of SPI changes in Connector AuthN Manager #High level changes: Substituted constants for Delimeters.

--- changes/db_impl/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SharepointAuthenticationManager.java
+++ changes/db_impl/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SharepointAuthenticationManager.java
@@ -38,115 +38,115 @@ import java.util.logging.Logger;
  * into the Connector Manager framework refer to
  * http://code.google.com/apis/searchappliance
  * /documentation/connectors/110/connector_dev/cdg_authentication.html
- *
+ * 
  * @author nitendra_thakur
  */
 public class SharepointAuthenticationManager implements AuthenticationManager {
-    Logger LOGGER = Logger.getLogger(SharepointAuthenticationManager.class.getName());
+	Logger LOGGER = Logger.getLogger(SharepointAuthenticationManager.class.getName());
 
-    SharepointClientContext sharepointClientContext = null;
+	SharepointClientContext sharepointClientContext = null;
 
-    /**
-     * @param inSharepointClientContext Context Information is required to
-     *            create the instance of this class
-     */
-    public SharepointAuthenticationManager(
-            final SharepointClientContext inSharepointClientContext)
-            throws SharepointException {
-        if (inSharepointClientContext == null) {
-            throw new SharepointException(
-                    "SharePointClientContext can not be null");
-        }
-        sharepointClientContext = (SharepointClientContext) inSharepointClientContext.clone();
-    }
+	/**
+	 * @param inSharepointClientContext Context Information is required to
+	 *            create the instance of this class
+	 */
+	public SharepointAuthenticationManager(
+			final SharepointClientContext inSharepointClientContext)
+			throws SharepointException {
+		if (inSharepointClientContext == null) {
+			throw new SharepointException(
+					"SharePointClientContext can not be null");
+		}
+		sharepointClientContext = (SharepointClientContext) inSharepointClientContext.clone();
+	}
 
-    /**
-     * Authenticates the user against the SharePoint server where Crawl URL
-     * specified during connector configuration is hosted
-     *
-     * @param identity AuthenticationIdentity object created by CM while
+	/**
+	 * Authenticates the user against the SharePoint server where Crawl URL
+	 * specified during connector configuration is hosted
+	 * 
+	 * @param identity AuthenticationIdentity object created by CM while
      *            delegating authentication to the connector. This
      *            corresponds to one specific search user
-     * @return AutheicationResponse Contains the authentication status for the
-     *         incoming identity
-     */
-    public AuthenticationResponse authenticate(
-            final AuthenticationIdentity identity)
-            throws RepositoryLoginException, RepositoryException {
-        if (sharepointClientContext == null) {
-            LOGGER.warning("SharePointClientContext is null. Authentication Failed.");
-            return null;
-        }
+	 * @return AutheicationResponse Contains the authentication status for the
+	 *         incoming identity
+	 */
+	public AuthenticationResponse authenticate(
+			final AuthenticationIdentity identity)
+			throws RepositoryLoginException, RepositoryException {
+		if (sharepointClientContext == null) {
+			LOGGER.warning("SharePointClientContext is null. Authentication Failed.");
+			return null;
+		}
 
-        GSBulkAuthorizationWS bulkAuth = null;
+		GSBulkAuthorizationWS bulkAuth = null;
 
-        final String user = identity.getUsername();
-        final String password = identity.getPassword();
-        String domain = identity.getDomain();
+		final String user = identity.getUsername();
+		final String password = identity.getPassword();
+		String domain = identity.getDomain();
 
         LOGGER.log(Level.INFO, "Received authN request for Username [ "
                 + user + " ], domain [ " + domain + " ]. ");
 
-        // If domain is not received as part of the authentication request, use
-        // the one from SharePointClientContext
-        if ((domain == null) || (domain.length() == 0)) {
-            domain = sharepointClientContext.getDomain();
-        }
+		// If domain is not received as part of the authentication request, use
+		// the one from SharePointClientContext
+		if ((domain == null) || (domain.length() == 0)) {
+			domain = sharepointClientContext.getDomain();
+		}
 
-        if (!Strings.isNullOrEmpty(password)) {
-            final String userName = Util.getUserNameWithDomain(user, domain);
-            LOGGER.log(Level.INFO, "Authenticating User: " + userName);
-            sharepointClientContext.setUsername(userName);
-            sharepointClientContext.setPassword(password);
-            try{
-                bulkAuth = new GSBulkAuthorizationWS(sharepointClientContext);
-            } catch (final Exception e) {
-                LOGGER.log(Level.SEVERE, "Failed to initialize GSBulkAuthorozationWS.", e);
-                return null;
-            }
-            /*
-             * If you can make a call to Web Service with the given credential,
-             * the user is valid user. This should not be assumed as a valid
+		if (!Strings.isNullOrEmpty(password)) {
+			final String userName = Util.getUserNameWithDomain(user, domain);
+			LOGGER.log(Level.INFO, "Authenticating User: " + userName);
+			sharepointClientContext.setUsername(userName);
+			sharepointClientContext.setPassword(password);
+			try {
+				bulkAuth = new GSBulkAuthorizationWS(sharepointClientContext);
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, "Failed to initialize GSBulkAuthorozationWS.", e);
+				return null;
+			}
+			/*
+			 * If you can make a call to Web Service with the given credential,
+			 * the user is valid user. This should not be assumed as a valid
              * SharePoint user. A valid user is any user who is identified on the
              * SharePoint server.The Google Services deployed on the SharePoint server
              * can be called with such user credentials.
-             *
+			 * 
              * If Authentication is successful get groups information from UserDataStore
              * data base for a given user and add it to AuthenticationResponse.             *
-             */
-            if (SPConstants.CONNECTIVITY_SUCCESS.equalsIgnoreCase(bulkAuth.checkConnectivity())) {
-                LOGGER.log(Level.INFO, "Authentication succeded for the user : ", userName);
-                if (null  != this.sharepointClientContext.getUserDataStoreDAO()) {
-                    return getAllGroupsForTheUser(user);
-                }
-            }
-        } else {
-            LOGGER.config("AuthN was not attempted as password is empty and groups are being returned.");
-            return getAllGroupsForTheUser(user);
-        }
-        LOGGER.log(Level.WARNING, "Authentication failed for " + user);
-        return new AuthenticationResponse(false, "", null);
-    }
+			 */
+			if (SPConstants.CONNECTIVITY_SUCCESS.equalsIgnoreCase(bulkAuth.checkConnectivity())) {
+				LOGGER.log(Level.INFO, "Authentication succeded for the user : ", userName);
+				if (null != this.sharepointClientContext.getUserDataStoreDAO()) {
+					return getAllGroupsForTheUser(user);
+				}
+			}
+		} else {
+			LOGGER.config("AuthN was not attempted as password is empty and groups are being returned.");
+			return getAllGroupsForTheUser(user);
+		}
+		LOGGER.log(Level.WARNING, "Authentication failed for " + user);
+		return new AuthenticationResponse(false, "", null);
+	}
 
     private AuthenticationResponse getAllGroupsForTheUser(String userName) throws SharepointException {
-        //Retrieving list of UserGroupMembership
-        List<UserGroupMembership> groupMemberList = this.sharepointClientContext.getUserDataStoreDAO().getAllMembershipsForUser(userName);
-        Set<String> groups = new HashSet<String>();
-        StringBuffer groupName;
-        boolean nameSpaceAppended = false;
-        if (sharepointClientContext.isAppendNamespaceInSPGroup()) {
-        	nameSpaceAppended = true;
-        }
-        for (UserGroupMembership userGroupMembership : groupMemberList) {
-        	if (!nameSpaceAppended) {
-        		groupName = new StringBuffer().append("[").append(userGroupMembership.getNamespace()).append("]").append(userGroupMembership.getGroupName());
-        	} else {
-                groupName = new StringBuffer().append(userGroupMembership.getGroupName());
-        	}
-            groups.add(groupName.toString());
-        }
+		// Retrieving list of UserGroupMembership
+		List<UserGroupMembership> groupMemberList = this.sharepointClientContext.getUserDataStoreDAO().getAllMembershipsForUser(userName);
+		Set<String> groups = new HashSet<String>();
+		StringBuffer groupName;
+		boolean nameSpaceAppended = false;
+		if (sharepointClientContext.isAppendNamespaceInSPGroup()) {
+			nameSpaceAppended = true;
+		}
+		for (UserGroupMembership userGroupMembership : groupMemberList) {
+			if (!nameSpaceAppended) {
+                groupName = new StringBuffer().append(SPConstants.LEFT_SQUARE_BRACKET).append(userGroupMembership.getNamespace()).append(SPConstants.RIGHT_SQUARE_BRACKET).append(userGroupMembership.getGroupName());
+			} else {
+				groupName = new StringBuffer().append(userGroupMembership.getGroupName());
+			}
+			groups.add(groupName.toString());
+		}
         LOGGER.log(Level.INFO, "Groups information for the user[ " + userName + " ]: " + groups);
         //Adding collection of groups data while creating AuthenticationResponse object.
-        return new AuthenticationResponse(true, "", groups);
-    }
+		return new AuthenticationResponse(true, "", groups);
+	}
 }
