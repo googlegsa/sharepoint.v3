137199d5d9f83016bb1e0f4e6073349e0e38a947
Saad Tambe <saad.tambe@gmail.com>
2011-09-26 15:01:33 +0000

    SharePoint Configuration UI Code Clean-Up.
    +Added USE_SP_SEARCH_VISIBILTY and USE_FEED_UNPUBLISHED_CONTENT as input
    checkbox elements while creating HTML for Configuration UI.
    +Removed SHAREPOINT_CRAWLING_OPTION as input checkbox elements while creating
    HTML for Configuration UI.
    -Remove appendnamespace elements from javascript.
    
    Performing null check for SP_SEARCH_VISIBILTY_OPTIONS and
    FEED_UNPUBLISHED_CONTENT field before retrieving it from configMap, required
    when configuring a new connector.

--- projects/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SharepointConnectorType.java
+++ projects/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SharepointConnectorType.java
@@ -285,21 +285,24 @@ public class SharepointConnectorType implements ConnectorType {
 					buf.append(rb.getString(SPConstants.AUTHZ_BY_CONNECTOR));
 				} else if (collator.equals(key, SPConstants.SHAREPOINT_CRAWLING_OPTION)) {
 					// displaying search visiblity option
+					String spSearchVisibilityValue = null;
+					String spSearchVisibilityKey = SPConstants.USE_SP_SEARCH_VISIBILITY;
 					buf.append(SPConstants.OPEN_ELEMENT);
 					buf.append(SPConstants.INPUT);
 					appendAttribute(buf, SPConstants.TYPE, SPConstants.CHECKBOX);
-					appendAttribute(buf, SPConstants.CONFIG_NAME, SPConstants.USE_SP_SEARCH_VISIBILITY);
-					appendAttribute(buf, SPConstants.CONFIG_ID, SPConstants.USE_SP_SEARCH_VISIBILITY);
+					appendAttribute(buf, SPConstants.CONFIG_NAME, spSearchVisibilityKey);
+					appendAttribute(buf, SPConstants.CONFIG_ID, spSearchVisibilityKey);
 					appendAttribute(buf, SPConstants.TITLE, rb.getString(SPConstants.USE_SP_SEARCH_VISIBILITY_HELP));
 					// The value can be true if its a pre-configured connector
 					// being edited and blank if the default connector form is
 					// being displayed.
 					if (configMap != null) {
-						value = configMap.get(SPConstants.USE_SP_SEARCH_VISIBILITY);
+						spSearchVisibilityValue = configMap.get(SPConstants.USE_SP_SEARCH_VISIBILITY);
 					} else {
-						value = "";
+						spSearchVisibilityValue = "";
 					}
-					if (value.equalsIgnoreCase("true") || value.length() == 0) {
+					if (spSearchVisibilityValue.equalsIgnoreCase("true")
+							|| spSearchVisibilityValue.length() == 0) {
 						appendAttribute(buf, SPConstants.CHECKED, Boolean.toString(true));
 					}
 					buf.append(" /" + SPConstants.CLOSE_ELEMENT);
@@ -313,19 +316,22 @@ public class SharepointConnectorType implements ConnectorType {
 							+ SPConstants.LABEL + SPConstants.CLOSE_ELEMENT);
 
 					// dispplaying unpublished content feed option
+					String feedUnpublishedContentValue = null;
+					String unpublishedContentKey = SPConstants.FEED_UNPUBLISHED_CONTENT;
 					buf.append(SPConstants.BREAK_LINE);
 					buf.append(SPConstants.OPEN_ELEMENT);
 					buf.append(SPConstants.INPUT);
 					appendAttribute(buf, SPConstants.TYPE, SPConstants.CHECKBOX);
-					appendAttribute(buf, SPConstants.CONFIG_NAME, SPConstants.FEED_UNPUBLISHED_CONTENT);
-					appendAttribute(buf, SPConstants.CONFIG_ID, SPConstants.FEED_UNPUBLISHED_CONTENT);
-					appendAttribute(buf, SPConstants.TITLE, rb.getString(SPConstants.FEED_UNPUBLISHED_CONTENT_LABEL));
+					appendAttribute(buf, SPConstants.CONFIG_NAME, unpublishedContentKey);
+					appendAttribute(buf, SPConstants.CONFIG_ID, unpublishedContentKey);
+					appendAttribute(buf, SPConstants.TITLE, rb.getString(unpublishedContentKey));
 					if (configMap != null) {
-						value = configMap.get(SPConstants.FEED_UNPUBLISHED_CONTENT);
+						feedUnpublishedContentValue = configMap.get(SPConstants.FEED_UNPUBLISHED_CONTENT);
 					} else {
-						value = "";
+						feedUnpublishedContentValue = "";
 					}
-					if (value.equalsIgnoreCase("true") || value.length() == 0) {
+					if (feedUnpublishedContentValue.equalsIgnoreCase("true")
+							|| feedUnpublishedContentValue.length() == 0) {
 						appendAttribute(buf, SPConstants.CHECKED, Boolean.toString(true));
 					} else {
 						appendAttribute(buf, SPConstants.UNCHECKED, Boolean.toString(false));
