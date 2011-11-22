0e2047ac23d84b25acca7038d961e0280ab26df4
John Lacey <jlacey@google.com>
2011-11-22 23:13:30 +0000

    Replaced use of the connector manager I18NUtil class with java.util.Locale to remove a trivial connector manager dependency from SharepointConnectorTypeTest. A less trivial dependency in IntegrationTest remains.
    Also extracted an assertFind method to dramatically shorten the code,
    removed extraneous uses of "this.", and removed calls to
    System.out.println.
    
    Code review: http://codereview.appspot.com/5413044/

--- projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/spiimpl/SharepointConnectorTypeTest.java
+++ projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/spiimpl/SharepointConnectorTypeTest.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2007 Google Inc.
+// Copyright 2007 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,10 +14,11 @@
 
 package com.google.enterprise.connector.sharepoint.spiimpl;
 
-import com.google.enterprise.connector.common.I18NUtil;
 import com.google.enterprise.connector.sharepoint.TestConfiguration;
 import com.google.enterprise.connector.sharepoint.client.SPConstants;
 import com.google.enterprise.connector.spi.ConfigureResponse;
+
+import junit.framework.TestCase;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,201 +28,85 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.TestCase;
-
 public class SharepointConnectorTypeTest extends TestCase {
-
   private List<String> keys;
   private Map<String, String> configMap;
   private SharepointConnectorType sharepointConnectorType;
   private boolean editMode = false;
 
   protected void setUp() throws Exception {
-    System.out.println("\n...Setting Up...");
-
-    this.configMap = new HashMap<String, String>();
-    this.configMap.putAll(TestConfiguration.getConfigMap());
-    this.keys = new ArrayList<String>();
-    this.keys.addAll(this.configMap.keySet());
+    configMap = new HashMap<String, String>();
+    configMap.putAll(TestConfiguration.getConfigMap());
+    keys = new ArrayList<String>();
+    keys.addAll(configMap.keySet());
     String isSelected = configMap.get(SPConstants.PUSH_ACLS);
     // if feedAcls option gets selected
     if (!isSelected.equalsIgnoreCase(SPConstants.TRUE)) {
-      this.editMode = SPConstants.EDIT_MODE;
+      editMode = SPConstants.EDIT_MODE;
     } else {
-      this.editMode = false;
+      editMode = false;
     }
-    this.sharepointConnectorType = new SharepointConnectorType();
-    assertNotNull(this.sharepointConnectorType);
-    System.out.println("SharepointConnectorType has been initialized successfully.");
-
-    this.sharepointConnectorType.setConfigKeys(this.keys);
-    System.out.println("Configuration keys has been loaded successfully.");
+    sharepointConnectorType = new SharepointConnectorType();
+    sharepointConnectorType.setConfigKeys(keys);
   }
 
   public void testValidateConfig() {
-    final ConfigureResponse configRes = this.sharepointConnectorType.validateConfig(this.configMap, I18NUtil.getLocaleFromStandardLocaleString("en"), null);
+    final ConfigureResponse configRes =
+        sharepointConnectorType.validateConfig(configMap, Locale.ENGLISH, null);
     assertNull(configRes);
   }
 
   public void testGetConfigForm() {
-    final ConfigureResponse configureResponse = this.sharepointConnectorType.getConfigForm(new Locale(
-        "en"));
+    final ConfigureResponse configureResponse =
+        sharepointConnectorType.getConfigForm(Locale.ENGLISH);
     final String initialConfigForm = configureResponse.getFormSnippet();
-    final boolean check = this.checkForExpectedFields(initialConfigForm);
-    assertTrue(check);
+    checkForExpectedFields(initialConfigForm);
     if (!editMode) {
-      checkForDisabledFileds(initialConfigForm);
+      checkForDisabledFields(initialConfigForm);
     }
   }
 
   public void testGetPopulatedConfigForm() {
-    final ConfigureResponse response = this.sharepointConnectorType.getPopulatedConfigForm(this.configMap, new Locale(
-        "test"));
+    final ConfigureResponse response =
+        sharepointConnectorType.getPopulatedConfigForm(configMap, new Locale("test"));
     final String populatedConfigForm = response.getFormSnippet();
-    final boolean check = this.checkForExpectedFields(populatedConfigForm);
-    assertTrue(check);
+    checkForExpectedFields(populatedConfigForm);
     if (!editMode) {
       checkForExpectedFields(populatedConfigForm);
     }
   }
 
-  private boolean checkForExpectedFields(final String configForm) {
-
-    System.out.println("Checking for SharePointURL field...");
-    String strPattern = "<input.*id=\"sharepointUrl\".*>";
-    Pattern pattern = Pattern.compile(strPattern);
-    Matcher match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for excludedURls field...");
-    strPattern = "<textarea.*id=\"excludedURls\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for AliasMap field...");
-    strPattern = "<input.*id=\"aliasMap\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for includedURls field...");
-    strPattern = "<textarea.*id=\"includedURls\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for domain field...");
-    strPattern = "<input.*id=\"domain\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for username field...");
-    strPattern = "<input.*id=\"username\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for mySiteBaseURL field...");
-    strPattern = "<input.*id=\"mySiteBaseURL\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for authorization field...");
-    strPattern = "<input.*id=\"authorization\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for useSPSearchVisibility field...");
-    strPattern = "<input.*id=\"useSPSearchVisibility\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for pushAcls field...");
-    strPattern = "<input.*id=\"pushAcls\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for appendNamespaceInSPGroup field...");
-    strPattern = "<input.*id=\"appendNamespaceInSPGroup\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for usernameFormatInAce field...");
-    strPattern = "<select.*id=\"usernameFormatInAce\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for groupnameFormatInAce field...");
-    strPattern = "<select.*id=\"groupnameFormatInAce\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for ldapServerHostAddress field...");
-    strPattern = "<input.*id=\"ldapServerHostAddress\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for portNumber field...");
-    strPattern = "<input.*id=\"portNumber\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for authenticationType field...");
-    strPattern = "<select.*id=\"authenticationType\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for connectMethod field...");
-    strPattern = "<select.*id=\"connectMethod\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for useCacheToStoreLdapUserGroupsMembership field...");
-    strPattern = "<input.*id=\"useCacheToStoreLdapUserGroupsMembership\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for initialCacheSize field...");
-    strPattern = "<input.*id=\"initialCacheSize\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for cacheRefreshInterval field...");
-    strPattern = "<input.*id=\"cacheRefreshInterval\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-    return true;
+  /** Asserts that the given pattern is found in the given string. */
+  private void assertFind(String strPattern, String configForm) {
+    assertTrue("Match for " + strPattern + " not found in: " + configForm,
+        Pattern.compile(strPattern).matcher(configForm).find());
   }
 
-  private boolean checkForDisabledFileds(final String configForm) {
+  private void checkForExpectedFields(final String configForm) {
+    assertFind("<input.*id=\"sharepointUrl\".*>", configForm);
+    assertFind("<textarea.*id=\"excludedURls\".*>", configForm);
+    assertFind("<input.*id=\"aliasMap\".*>", configForm);
+    assertFind("<textarea.*id=\"includedURls\".*>", configForm);
+    assertFind("<input.*id=\"domain\".*>", configForm);
+    assertFind("<input.*id=\"username\".*>", configForm);
+    assertFind("<input.*id=\"mySiteBaseURL\".*>", configForm);
+    assertFind("<input.*id=\"authorization\".*>", configForm);
+    assertFind("<input.*id=\"useSPSearchVisibility\".*>", configForm);
+    assertFind("<input.*id=\"pushAcls\".*>", configForm);
+    assertFind("<input.*id=\"appendNamespaceInSPGroup\".*>", configForm);
+    assertFind("<select.*id=\"usernameFormatInAce\".*>", configForm);
+    assertFind("<select.*id=\"groupnameFormatInAce\".*>", configForm);
+    assertFind("<input.*id=\"ldapServerHostAddress\".*>", configForm);
+    assertFind("<input.*id=\"portNumber\".*>", configForm);
+    assertFind("<select.*id=\"authenticationType\".*>", configForm);
+    assertFind("<select.*id=\"connectMethod\".*>", configForm);
+    assertFind("<input.*id=\"useCacheToStoreLdapUserGroupsMembership\".*>", configForm);
+    assertFind("<input.*id=\"initialCacheSize\".*>", configForm);
+    assertFind("<input.*id=\"cacheRefreshInterval\".*>", configForm);
+  }
 
-    System.out.println("Checking for cacheRefreshInterval field...");
-    String strPattern = "<input.*id=\"cacheRefreshInterval\" disabled=\"true\".*>";
-    Pattern pattern = Pattern.compile(strPattern);
-    Matcher match = pattern.matcher(configForm);
-    assertTrue(match.find());
-
-    System.out.println("Checking for initialCacheSize field...");
-    strPattern = "<input.*id=\"initialCacheSize\".*disabled=\"true\".*>";
-    pattern = Pattern.compile(strPattern);
-    match = pattern.matcher(configForm);
-    assertTrue(match.find());
-    return true;
+  private void checkForDisabledFields(final String configForm) {
+    assertFind("<input.*id=\"cacheRefreshInterval\" disabled=\"true\".*>", configForm);
+    assertFind("<input.*id=\"initialCacheSize\".*disabled=\"true\".*>", configForm);
   }
 }
