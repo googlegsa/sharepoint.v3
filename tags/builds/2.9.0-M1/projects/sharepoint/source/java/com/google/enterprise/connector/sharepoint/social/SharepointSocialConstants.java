8867b5461457dd43abf227b2d121e77f8c89587a
tvartak@google.com <tvartak@google.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2012-02-28 21:21:11 +0000

    Tagging the 2.9.0-M1 Release

---
+++ tags/builds/2.9.0-M1/projects/sharepoint/source/java/com/google/enterprise/connector/sharepoint/social/SharepointSocialConstants.java
@@ -0,0 +1,61 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.sharepoint.social;
+
+public class SharepointSocialConstants {
+  /**
+   * SharePoint service URL for userprofile service.
+   */
+  public static final String SHAREPOINT_USERPROFILESERVICE = "/_vti_bin/UserProfileService.asmx";
+  /**
+   * Standard SharePoint property.
+   */
+  public static final String SHAREPOINT_USERKEYDEFAULT = "AccountName";
+  /**
+   * Standard SharePoint property.
+   */
+  public static final String SHAREPOINT_USERCONTENTDEFAULT = "PreferredName";
+  /**
+   * Mapped meta in GSA.
+   */
+  public static final String PROPNAME_ACCOUNTNAME = 
+      "google.userprofile.AccountName";
+  /**
+   * Mapped meta in GSA.
+   */
+  public static final String PROPNME_PREFERREDNAME = 
+      "google.userprofile.PreferredName";
+  /**
+   * Standard SharePoint property.
+   */
+  public static final String PROPNAME_RESPONSIBILITY = "SPS_Responsibility";
+  /**
+   * Standard SharePoint property.
+   */
+  public static final String PROPNAME_SKILL = "SPS_Skills";
+  /**
+   * Standard SharePoint property.
+   */
+  public static final String PROPNAME_PASTPROJECTS = "SPS_PastProjects";
+  /**
+   * Default port for GSA admin controller.
+   */
+  public static final int DEFAULT_GSAADMINPORT = 8000;
+  /**
+   * Prefix used for google connector Url.
+   */
+  public static final String PREFIX_GOOGLECONNECTORPATTERN = 
+      "^googleconnector://";
+}
