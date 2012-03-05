5fa5c13f370ae8257b92b15b7fb1b244512a569b
tvartak@google.com <tvartak@google.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2012-03-05 22:56:50 +0000

    Branching 2.8.2 to 2.8.2-X1 for Fix Issue 6057626

---
+++ branches/dev/2.8.2-X1/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/spiimpl/SPDocumentTest.java
@@ -0,0 +1,95 @@
+//Copyright 2007 Google Inc.
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+package com.google.enterprise.connector.sharepoint.spiimpl;
+
+import com.google.enterprise.connector.sharepoint.TestConfiguration;
+import com.google.enterprise.connector.sharepoint.client.SPConstants;
+import com.google.enterprise.connector.sharepoint.client.SharepointClientContext;
+import com.google.enterprise.connector.sharepoint.client.SPConstants.FeedType;
+import com.google.enterprise.connector.sharepoint.client.SPConstants.SPType;
+import com.google.enterprise.connector.spi.Property;
+import com.google.enterprise.connector.spi.SpiConstants;
+
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import com.sun.jndi.toolkit.url.UrlUtil;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class SPDocumentTest extends TestCase {
+
+  SPDocument doc;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    SharepointClientContext spContext = TestConfiguration.initContext();
+    List<SPDocument> allDocs = TestConfiguration.initState(spContext).lookupList(TestConfiguration.Site1_URL, TestConfiguration.Site1_List1_GUID).getCrawlQueue();
+    this.doc = allDocs.get(0);
+    this.doc.setSharepointClientContext(spContext);
+    this.doc.setContentDwnldURL(doc.getUrl());
+    try {
+      String str = UrlUtil.encode(doc.getUrl(), "UTF-8");
+      String charset = new GetMethod(str).getParams().getUriCharset();
+      URI uri = new URI(doc.getUrl(), true, charset);
+      System.out.println(str);
+      System.out.println(uri.toString());
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public void testCompare() {
+    final SPDocument tmpdoc1 = new SPDocument("1", "HTTP://MYCOMP.COM",
+        Calendar.getInstance(), SPConstants.NO_AUTHOR, SPConstants.NO_OBJTYPE,
+        SPConstants.PARENT_WEB_TITLE, FeedType.CONTENT_FEED, SPType.SP2007);
+    final SPDocument tmpdoc2 = new SPDocument("2", "HTTP://MYCOMP.COM",
+        Calendar.getInstance(), SPConstants.NO_AUTHOR, SPConstants.NO_OBJTYPE,
+        SPConstants.PARENT_WEB_TITLE, FeedType.CONTENT_FEED, SPType.SP2007);
+    final List<SPDocument> testSet = new ArrayList<SPDocument>();
+    testSet.add(tmpdoc1);
+    testSet.add(tmpdoc2);
+    Collections.sort(testSet);
+    System.out.println(testSet);
+  }
+
+  public final void testFindProperty() {
+    System.out.println("Testing findProperty()..");
+    try {
+      final Property prop = this.doc.findProperty(SpiConstants.PROPNAME_DOCID);
+      assertNotNull(prop);
+      System.out.println("[ findProperty(() ] Test passd");
+    } catch (final Exception e) {
+      System.out.println("[ findProperty(() ] Test failed");
+    }
+  }
+
+  public final void testDownloadContents() {
+    System.out.println("Testing downloadContents()..");
+    try {
+      final String responseCode = this.doc.downloadContents();
+      assertEquals(responseCode, SPConstants.CONNECTIVITY_SUCCESS);
+    } catch (final Exception e) {
+      assertTrue(false);
+    }
+    System.out.println("[ downloadContents(() ] Test passd");
+  }
+
+}
