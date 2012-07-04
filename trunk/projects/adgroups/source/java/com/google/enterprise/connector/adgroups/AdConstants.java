d4e620f583ee16351db690287497d5e2239237d9
ondrejnovak@google.com <ondrejnovak@google.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2012-07-04 12:33:15 +0000

    Crawl only person user objects
    http://codereview.appspot.com/6350065/

--- trunk/projects/adgroups/source/java/com/google/enterprise/connector/adgroups/AdConstants.java
+++ trunk/projects/adgroups/source/java/com/google/enterprise/connector/adgroups/AdConstants.java
@@ -40,7 +40,7 @@ public class AdConstants {
   // and ADS_GROUP_TYPE_SECURITY_ENABLED = 2147483648.
   public static final String LDAP_QUERY =
       "(|(&(objectClass=group)(groupType:1.2.840.113556.1.4.803:=2147483648))"
-          + "(objectClass=user))";
+          + "(&(objectClass=user)(objectCategory=person)))";
   public static final String PARTIAL_QUERY_START =
       "(&(" + AdConstants.ATTR_USNCHANGED + ">=";
   public static final String PARTIAL_QUERY_END =
