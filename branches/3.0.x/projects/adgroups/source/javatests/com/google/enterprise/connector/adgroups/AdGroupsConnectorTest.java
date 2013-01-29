9c5c3a9fff74abf1f5791c2dd0fbb39c6d52ecd2
jlacey@google.com <jlacey@google.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2013-01-29 08:45:00 +0000

    Merged all unmerged AD groups trunk changes to the 3.0.x release branch: r1997, r2011, r2021, r2043, r2051, and r2052.

--- branches/3.0.x/projects/adgroups/source/javatests/com/google/enterprise/connector/adgroups/AdGroupsConnectorTest.java
+++ branches/3.0.x/projects/adgroups/source/javatests/com/google/enterprise/connector/adgroups/AdGroupsConnectorTest.java
@@ -21,6 +21,7 @@ import com.google.enterprise.connector.spi.AuthenticationResponse;
 import com.google.enterprise.connector.spi.Principal;
 import com.google.enterprise.connector.spi.Session;
 import com.google.enterprise.connector.spi.SimpleAuthenticationIdentity;
+import com.google.enterprise.connector.spi.TraversalManager;
 
 import junit.framework.TestCase;
 
@@ -142,7 +143,7 @@ public class AdGroupsConnectorTest extends TestCase {
       ad.createOu(ou);
       Set<String> names = new HashSet<String>();
       List<AdTestEntity> namespace = new ArrayList<AdTestEntity>();
-      Random random = new Random(TestConfiguration.seed);
+      Random random = new Random(TestConfiguration.seed + 1);
       AdTestEntity group = new AdTestEntity(names, namespace, random, 1);
       AdTestEntity user = new AdTestEntity(names, namespace, random);
       ad.createGroup(false, group, ou);
@@ -201,7 +202,7 @@ public class AdGroupsConnectorTest extends TestCase {
       ad.createOu(ou);
       Set<String> names = new HashSet<String>();
       List<AdTestEntity> namespace = new ArrayList<AdTestEntity>();
-      Random random = new Random(TestConfiguration.seed);
+      Random random = new Random(TestConfiguration.seed + 2);
       AdTestEntity user = new AdTestEntity(names, namespace, random);
       // create the user
       ad.createUser(false, user, ou);
@@ -279,10 +280,154 @@ public class AdGroupsConnectorTest extends TestCase {
         }
 
         for (AdTestEntity e : groupsCorrect) {
-          assertTrue(groups.contains(ad.getnETBIOSName()
-              + AdConstants.BACKSLASH + e.sAMAccountName));
+          String group = ad.getnETBIOSName() + AdConstants.BACKSLASH
+              + e.sAMAccountName;
+          assertTrue("Group [" + group + "] must be found in  [" + groups + "]",
+              groups.contains(group));
         }
       }
+    }
+  }
+
+  public void testMultipleThreads() throws Exception {
+    for (String dbType : TestConfiguration.dbs.keySet()) {
+      AdGroupsConnector con = new AdGroupsConnector();
+      con.setMethod("SSL");
+      con.setHostname(TestConfiguration.d1hostname);
+      con.setPort(Integer.toString(TestConfiguration.d1port));
+      con.setPrincipal(TestConfiguration.d1principal);
+      con.setPassword(TestConfiguration.d1password);
+
+      con.setDataSource(dbType, TestConfiguration.dbs.get(dbType));
+      Session s = con.login();
+      AuthenticationManager am = s.getAuthenticationManager();
+      TraversalManager tm = s.getTraversalManager();
+      tm.startTraversal();
+      
+      // four threads which will be crawling the database simultaneously
+      List<TestThread> threads = new ArrayList<TestThread>();
+      for (int i = 0; i < 4; ++i) {
+        TestThread t = new TestThread(TestConfiguration.d1hostname,
+            TestConfiguration.d1port,
+            TestConfiguration.d1principal,
+            TestConfiguration.d1password,
+            dbType, TestConfiguration.dbs.get(dbType), 2);
+        t.start();
+        threads.add(t);
+      }
+
+      String username = TestConfiguration.d1principal.split("\\\\")[1];
+      AuthenticationResponse response = am.authenticate(
+          new SimpleAuthenticationIdentity(username));
+      // we will expect to find exactly the same number of groups during crawl
+      int constGroups = response.getGroups().size();
+      boolean finished = false;
+
+      // Run Authentication every 200ms and measure if it was faster than 3 sec
+      while (!finished) {
+        Thread.sleep(200);
+        long start = System.currentTimeMillis();
+        response = am.authenticate(
+          new SimpleAuthenticationIdentity(username));
+        long diff = System.currentTimeMillis() - start; 
+        assertTrue("Less than three seconds [" + diff + "]" , diff < 3000);
+        assertTrue("Principal valid: " + TestConfiguration.d1principal,
+            response.isValid());
+        int groups = response.getGroups().size(); 
+        assertEquals("Group number constant start: [" + constGroups
+            + "] now: [" + groups + "]" , constGroups, groups);
+        finished = true;
+        for (TestThread t : threads) {
+          if (t.isAlive()) {
+            finished = false;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  public void testResurrection() throws Exception {
+    for (String dbType : TestConfiguration.dbs.keySet()) {
+      // Initialize AD
+      AdTestServer ad = new AdTestServer(
+          Method.SSL,
+          TestConfiguration.d1hostname,
+          TestConfiguration.d1port,
+          TestConfiguration.d1principal,
+          TestConfiguration.d1password);
+      ad.initialize();
+
+      // prepare ou
+      String ou = TestConfiguration.testOu + "_resurrection";
+      ad.deleteOu(ou);
+      ad.createOu(ou);
+
+      // prepare names
+      Set<String> names = new HashSet<String>();
+      List<AdTestEntity> namespace = new ArrayList<AdTestEntity>();
+      Random random = new Random(TestConfiguration.seed + 3);
+
+      // create user
+      AdTestEntity user = new AdTestEntity(names, namespace, random);
+      ad.createUser(false, user, ou);
+
+      // create group
+      AdTestEntity group = new AdTestEntity(names, namespace, random, -1);
+      ad.createGroup(false, group, ou);
+      group.children.add(user);
+      ad.setMembers(false, group);
+
+      // crawl AD
+      AdGroupsConnector con = new AdGroupsConnector();
+      con.setMethod("SSL");
+      con.setHostname(TestConfiguration.d1hostname);
+      con.setPort(Integer.toString(TestConfiguration.d1port));
+      con.setPrincipal(TestConfiguration.d1principal);
+      con.setPassword(TestConfiguration.d1password);
+      con.setDataSource(dbType, TestConfiguration.dbs.get(dbType));
+      Session s = con.login();
+      s.getTraversalManager().resumeTraversal(null);
+
+      // delete the user, create same one with different GUID
+      ad.initialize();
+      ad.deleteEntity(user);
+      ad.createUser(false, user, ou);
+
+      // recrawl AD
+      s.getTraversalManager().resumeTraversal(null);
+
+      AuthenticationResponse response = s.getAuthenticationManager()
+          .authenticate(new SimpleAuthenticationIdentity(
+              user.sAMAccountName, TestConfiguration.password));
+
+      assertTrue("Authentication must succeed for resurrected user",
+          response.isValid());
+
+      assertFalse(
+          "New user mustn't belong to the group he belonged to prior deletion",
+          response.getGroups().contains(new Principal(group.sAMAccountName)));
+
+      // add new user as member to the group
+      ad.initialize();
+      group.children.add(user);
+      ad.setMembers(false, group);
+      s.getTraversalManager().resumeTraversal(null);
+
+      // delete the group, create the same one with different GUID
+      ad.deleteEntity(group);
+      ad.createGroup(false, group, ou);
+      s.getTraversalManager().resumeTraversal(null);
+
+      response = s.getAuthenticationManager().authenticate(
+          new SimpleAuthenticationIdentity(
+              user.sAMAccountName, TestConfiguration.password));
+
+      assertTrue("User from resurrected group can be authenticated",
+          response.isValid());
+
+      assertFalse("User must not belong to resurrected group",
+          response.getGroups().contains(new Principal(group.sAMAccountName)));
     }
   }
 }
