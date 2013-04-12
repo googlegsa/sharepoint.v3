6e3ebaf5c9b38b0d53fbd86403cf1e22d45c81e0
mifern@google.com <mifern@google.com@ea989cb3-4a63-c47a-0c2e-51e1f179f7af>
2013-04-12 21:31:53 +0000

    Part 3 of sp2003.UserProfileWS re-factoring. This CL updates the tests to use SiteDataHelper instead of SiteDataWS.
    Code review: https://codereview.appspot.com/7719049/

--- trunk/projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/wsclient/soap/sp2003/SPUserProfileWSTest.java
+++ trunk/projects/sharepoint/source/javatests/com/google/enterprise/connector/sharepoint/wsclient/soap/sp2003/SPUserProfileWSTest.java
@@ -14,9 +14,9 @@
 
 package com.google.enterprise.connector.sharepoint.wsclient.soap.sp2003;
 
-import com.google.enterprise.connector.sharepoint.TestConfiguration;
+import com.google.enterprise.connector.sharepoint.client.UserProfile2003Helper;
 import com.google.enterprise.connector.sharepoint.client.SharepointClientContext;
-import com.google.enterprise.connector.sharepoint.wsclient.soap.sp2003.SPUserProfileWS;
+import com.google.enterprise.connector.sharepoint.TestConfiguration;
 import com.google.enterprise.connector.spi.RepositoryException;
 
 import java.net.MalformedURLException;
@@ -25,37 +25,30 @@ import java.util.Set;
 import junit.framework.TestCase;
 
 /**
- * Test the functionality of UserProfile web service (SP2007).
+ * Test the functionality of UserProfile web service (SP2003).
  *
  * @author amit_kagrawal
  */
 public class SPUserProfileWSTest extends TestCase {
   SharepointClientContext sharepointClientContext;
-  SPUserProfileWS userProfileWS;
+  UserProfile2003Helper userProfile;
 
   protected void setUp() throws Exception {
-    System.out.println("\n...Setting Up...");
-    System.out.println("Initializing SharepointClientContext ...");
     this.sharepointClientContext = TestConfiguration.initContext();
     assertNotNull(this.sharepointClientContext);
     sharepointClientContext.setIncluded_metadata(TestConfiguration.whiteList);
     sharepointClientContext.setExcluded_metadata(TestConfiguration.blackList);
 
-    System.out.println("Initializing SPUserProfileWS ...");
-    this.userProfileWS = new SPUserProfileWS(this.sharepointClientContext);
+    this.userProfile = new UserProfile2003Helper(this.sharepointClientContext);
   }
 
   public void testIsSPS() throws MalformedURLException, RepositoryException {
-    System.out.println("Testing isSPS()...");
-    this.userProfileWS.isSPS();
-    System.out.println("[ isSPS() ] Test Completed.");
+    assertTrue(userProfile.isSPS());
   }
 
   public void testGetPersonalSiteList() throws MalformedURLException,
       RepositoryException {
-    System.out.println("Testing getPersonalSiteList()...");
-    final Set<String> items = this.userProfileWS.getPersonalSiteList();
+    final Set<String> items = userProfile.getPersonalSiteList();
     assertNotNull(items);
-    System.out.println("[ getPersonalSiteList() ] Test Passed.");
   }
 }
