53feaf43f113d8f92420613183f5c824b396e9cb
Tanmay Vartak <tvartak@google.com>
2013-05-01 18:32:07 +0000

    Fix for b/8538403 By sending delete record for unpublished folders, connector broke the ACL chain for child Code Review https://codereview.appspot.com/8536047/

--- projects/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SPDocument.java
+++ projects/sharepoint/source/java/com/google/enterprise/connector/sharepoint/spiimpl/SPDocument.java
@@ -160,6 +160,8 @@ public class SPDocument implements Document, Comparable<SPDocument> {
   
   // flag to indicate if document is public
   private boolean publicDocument;
+  // flag to indicate if it is empty document
+  private boolean emptyDocument;
 
   /**
    * @return the toBeFed
@@ -713,10 +715,12 @@ public class SPDocument implements Document, Comparable<SPDocument> {
   public Set<String> getPropertyNames() throws RepositoryException {
     final Set<String> names = new HashSet<String>();
     ArrayList<String> candidates = new ArrayList<String>();
-    candidates.add(SPConstants.OBJECT_TYPE);
-    candidates.add(SPConstants.LIST_GUID);
-    candidates.add(SPConstants.SPAUTHOR);
-    candidates.add(SPConstants.PARENT_WEB_TITLE);
+    if (!isEmptyDocument()) {
+      candidates.add(SPConstants.OBJECT_TYPE);
+      candidates.add(SPConstants.LIST_GUID);
+      candidates.add(SPConstants.SPAUTHOR);
+      candidates.add(SPConstants.PARENT_WEB_TITLE);
+    }
     if (null != documentType) {
       names.add(SpiConstants.PROPNAME_DOCUMENTTYPE);
     }
@@ -763,14 +767,16 @@ public class SPDocument implements Document, Comparable<SPDocument> {
         names.add(SpiConstants.PROPNAME_ACLDENYGROUPS);
       }
     }
-    // Add "extra" metadata fields, including those added by user to the
-    // documentMetadata List for matching against patterns
-    for (final Iterator<Attribute> iter = getAllAttrs().iterator(); iter.hasNext();) {
-      final Attribute attr = iter.next();
-      candidates.add(attr.getName().toString());
-    }
-    if (null != title) {
-      names.add(SpiConstants.PROPNAME_TITLE);
+    if (!isEmptyDocument()) {
+      // Add "extra" metadata fields, including those added by user to the
+      // documentMetadata List for matching against patterns
+      for (final Iterator<Attribute> iter = getAllAttrs().iterator(); iter.hasNext();) {
+        final Attribute attr = iter.next();
+        candidates.add(attr.getName().toString());
+      }
+      if (null != title) {
+        names.add(SpiConstants.PROPNAME_TITLE);
+      }
     }
     ArrayList<Pattern> excludedMetadataPatterns = sharepointClientContext.getExcluded_metadata();
     // Add only those metadata attributes which do not come under excluded
@@ -843,6 +849,12 @@ public class SPDocument implements Document, Comparable<SPDocument> {
     }
     LOGGER.config("Document URL [ " + contentDwnldURL
         + " is getting processed for contents");
+    if (isEmptyDocument()) {
+      LOGGER.config("Document URL [" + contentDwnldURL
+          + "] is empty document");
+      return new SPContent("empty",
+          docContentType, docContentStream); 
+    }
     int responseCode = 0;
     boolean downloadContent = true;
     if (getFileSize() > 0
@@ -1163,6 +1175,13 @@ public class SPDocument implements Document, Comparable<SPDocument> {
     this.publicDocument = publicDocument;
   }
 
+  public boolean isEmptyDocument() {
+    return emptyDocument;
+  }
+
+  public void setEmptyDocument(boolean emptyDocument) {
+    this.emptyDocument = emptyDocument;
+  }
   @VisibleForTesting
   class SPContent {
     private final String status;
